ae5d587233517a6d4ff7e02c6afa46f59f41722c
Howard Richards <dev@conficient.com>
2019-05-01 11:36:19 +0100

    Create a simple DynamicComponent that doesn't do much

---
+++ BlazorDynamicList.Client/DynamicComponent.cs
@@ -0,0 +1,30 @@
+﻿using BaseClasses;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.RenderTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDynamicList.Client
+{
+    public class DynamicComponent : ComponentBase
+    {
+        public DynamicComponent()
+        {
+        }
+        
+        
+        [Parameter]
+        private ProductBase Product { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+
+            builder.OpenElement(0, "p");
+            builder.AddContent(1, Product?.Name);
+            builder.CloseElement();
+        }
+    }
+}
